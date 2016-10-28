d7c80bde327cf742d36ccbb6ee71492348e82151
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-10-28 19:09:50 +0200

    added prefabs

--- Assets/PCScreenObject.cs
+++ Assets/PCScreenObject.cs
@@ -13,16 +13,18 @@ public class PCScreenObject : MonoBehaviour {
     private PcScreenWindow m_associtatedWindow;
     private Image m_iconImage;
     private Text m_iconText;
-    private bool m_windowOpened;
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
-
-        m_windowOpened = false;
         m_iconImage = GetComponent<Image>();
         m_iconText = transform.GetChild(0).GetComponent<Text>();
         m_iconImage.sprite = m_iconSprite;
         m_iconText.text = m_IconName;
-        m_associtatedWindow = Instantiate(m_windowPrefab).GetComponent<PcScreenWindow>();
+
+        if(m_windowPrefab!=null)
+            m_associtatedWindow = Instantiate(m_windowPrefab).GetComponent<PcScreenWindow>();
+
+
 	}
 	
 	// Update is called once per frame
@@ -32,7 +34,8 @@ public class PCScreenObject : MonoBehaviour {
 
     public void OpenWindow()
     {
-        m_associtatedWindow.Open();
+        if(m_associtatedWindow.State != PcWindowState.Open)
+            m_associtatedWindow.Open();
 
     }
 
