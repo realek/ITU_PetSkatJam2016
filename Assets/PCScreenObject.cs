990cc7e25e13317da0c353affd3e0364d1a6ecc8
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-10-28 18:06:05 +0200

    Added PcScreenObject implementation Added basic PcScreenWindow implementation

--- Assets/PCScreenObject.cs
+++ Assets/PCScreenObject.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PCScreenObject : MonoBehaviour {
 
+    [SerializeField]
+    private Sprite m_iconSprite;
+    [SerializeField]
+    private string m_IconName;
+    [SerializeField]
+    private GameObject m_windowPrefab;
+    private PcScreenWindow m_associtatedWindow;
+    private Image m_iconImage;
+    private Text m_iconText;
+    private bool m_windowOpened;
 	// Use this for initialization
 	void Start () {
-	
+
+        m_windowOpened = false;
+        m_iconImage = GetComponent<Image>();
+        m_iconText = transform.GetChild(0).GetComponent<Text>();
+        m_iconImage.sprite = m_iconSprite;
+        m_iconText.text = m_IconName;
+        m_associtatedWindow = Instantiate(m_windowPrefab).GetComponent<PcScreenWindow>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void OpenWindow()
+    {
+        m_associtatedWindow.Open();
+
+    }
+
 }
---
+++ Assets/PcScreenWindow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PcWindowState
+{
+    None,
+    Open,
+    Closed,
+    Minimized
+}
+
+public class PcScreenWindow : MonoBehaviour {
+
+    private PcWindowState m_state;
+    public PcWindowState State
+    {
+        get
+        {
+            return m_state;
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+
+        m_state = PcWindowState.Closed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Open()
+    {
+        m_state = PcWindowState.Open;
+    }
+
+    public void Close()
+    {
+        m_state = PcWindowState.Closed;
+    }
+
+    public void Minimize()
+    {
+        m_state = PcWindowState.Open;
+    }
+}
