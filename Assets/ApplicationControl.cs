7e4c973ab1c96273481d44540c87807dc6626f44
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-10-30 15:42:27 +0100

    Added taskbar functionality

---
+++ Assets/ApplicationControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationControl : MonoBehaviour {
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/PcTaskbar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PcTaskbar : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_StartMenu;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ToggleStartMenu()
+    {
+        if (m_StartMenu.activeSelf)
+            m_StartMenu.SetActive(false);
+        else
+            m_StartMenu.SetActive(true);
+    }
+}
