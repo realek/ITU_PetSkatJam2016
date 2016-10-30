4d24fe818cd61adcaa5a0cd694a7bdbf40d50a89
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-10-30 15:30:31 +0100

    Added trust Added email functionality Added ability to lose funds Added ability to be Scammed

---
+++ Assets/Email.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+
+public enum EmailType
+{
+    None,
+    Fake,
+    Real
+}
+public class Email : MonoBehaviour {
+
+    [SerializeField]
+    private string m_subject = "This is a template E-mail";
+    [SerializeField]
+    private bool m_seen = false;
+    public bool isSeen
+    {
+        get
+        {
+            return m_seen;
+        }
+    }
+    [SerializeField]
+    private EmailType m_type;
+    private Text m_text;
+    private Button m_button;
+    [SerializeField]
+    private Sprite m_EmailContent;
+    [SerializeField]
+    private Color m_unSeenColorNormal;
+    [SerializeField]
+    private Color m_unSeenColorHLit;
+    private ColorBlock m_colorBkup;
+    [SerializeField]
+    private GameObject m_content;
+    private Image m_contentDisplay;
+    [SerializeField]
+    private float m_trustPriceDKK;
+    [SerializeField]
+    private bool m_isApartment = false;
+
+    public bool ApartmentFound
+    {
+        get
+        {
+            return m_isApartment;
+        }
+    }
+    public float Price
+    {
+        get
+        {
+            return m_trustPriceDKK;
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+
+        m_button = GetComponent<Button>();
+        m_text = transform.GetChild(0).GetComponent<Text>();
+        m_text.text = m_subject;
+        ColorBlock colorB = m_button.colors;
+        m_colorBkup = m_button.colors;
+        colorB.normalColor = m_unSeenColorNormal;
+        colorB.highlightedColor = m_unSeenColorHLit;
+        m_button.colors = colorB;
+        m_contentDisplay = m_content.transform.GetChild(0).GetComponent<Image>();
+	}
+
+    public void AddContentDisplayFunction(UnityAction action)
+    {
+        m_button.onClick.AddListener(action);
+    }
+
+    public void Seen()
+    {
+        m_button.colors = m_colorBkup;
+        m_seen = true;
+        m_contentDisplay.sprite = m_EmailContent;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/EmailContent.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class EmailContent : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_mailList;
+    private Email[] m_mails;
+    private bool m_checkedMail = false;
+    private Email m_currentMail;
+    [SerializeField]
+    private GameObject m_trust;
+	// Use this for initialization
+	void Start () {
+
+        m_mails = new Email[m_mailList.transform.childCount];
+
+        for (int i = 0; i < m_mailList.transform.childCount; i++)
+        {
+            m_mails[i] = m_mailList.transform.GetChild(i).GetComponent<Email>();
+        }
+
+        m_trust.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckIfAllSeen();
+	
+	}
+
+    public void TrustEmail()
+    {
+        EventManager.Bank.WithDrawFunds(m_currentMail.Price);
+        if (m_currentMail.ApartmentFound)
+        {
+            EventManager.Notes.CompleteApartment();
+        }
+    }
+    public void SetCurrentMail(Email em)
+    {
+        m_currentMail = em;
+    }
+
+    void CheckIfAllSeen()
+    {
+        if (m_currentMail != null)
+        {
+            if (!m_trust.activeSelf)
+            {
+                m_trust.SetActive(true);
+            }
+
+        }
+
+        int count = 0;
+        for (int i = 0; i < m_mails.Length; i++)
+        {
+
+            if (m_mails[i].isSeen)
+            {
+                count++;
+            }
+        }
+
+        if (count == m_mails.Length && !m_checkedMail)
+        {
+            EventManager.Notes.CompleteMail();
+            m_checkedMail = true;
+        }
+    }
+}
